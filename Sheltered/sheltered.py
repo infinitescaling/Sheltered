68077d7152e378737f552213600bd47cea377768
Scott C <scott.caling@gmail.com>
2017-08-18 13:54:05 +0900

    For heroku use

--- Sheltered/sheltered.py
+++ Sheltered/sheltered.py
@@ -2,9 +2,7 @@ import os
 
 def find_picture():
     picture_list = []
-    print('cwd file path is ' + os.getcwd())
-    print('Filepath is ' + os.path.join(os.getcwd(),"Sheltered/static/images/"))
-    for filename in os.listdir(os.path.join(os.getcwd(),"Sheltered/static/images/")):
+    for filename in os.listdir(os.path.join(os.getcwd(),"static/images/")):
         if filename.endswith(".jpg") or filename.endswith(".png"):
             picture_list.append(os.path.join("/static/images",filename))
     return picture_list
