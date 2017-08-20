293b424e32868d8e802e9890e7444db3d57ecf7e
Scott C <scott.caling@gmail.com>
2017-08-21 08:32:42 +0900

    Fixing images, image batch #3

--- Sheltered/sheltered.py
+++ Sheltered/sheltered.py
@@ -2,7 +2,7 @@ import os
 
 def find_picture():
     picture_list = []
-    for filename in os.listdir(os.path.join(os.getcwd(),"static/images/")):
+    for filename in os.listdir(os.path.join(os.getcwd(),"Sheltered/static/images/")):
         if filename.endswith(".jpg") or filename.endswith(".png"):
             picture_list.append(os.path.join("/static/images",filename))
     return picture_list
